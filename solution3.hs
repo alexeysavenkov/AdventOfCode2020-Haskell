92b96566befa9dc0d74aa7fc65278bb90419e210
Alexey Savenkov <alexey.savenkov7@gmail.com>
2021-01-02 00:15:24 +0200

    Update solution3.hs

--- solution3.hs
+++ solution3.hs
@@ -1,5 +1,8 @@
 import Data.List
 
-f1::Int->[String]->Int
-f1 _ [] = 0
-f1 pos (row:rest) = (if (row !! (pos `mod` (length row))) == '#' then 1 else 0) + f (pos + 3) rest
+f::Int->Int->Int->[String]->Int
+f _ _ _ [] = 0
+f slopex slopey pos (row:rest) = (if (row !! (pos `mod` (length row))) == '#' then 1 else 0) + f slopex slopey (pos + slopex) (drop (slopey - 1) rest)
+
+f2::[String]->Integer
+f2 m = product $ map (\(x,y) -> fromIntegral $ f x y 0 m) [(1,1),(3,1),(5,1),(7,1),(1,2)]
