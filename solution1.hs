57d2bf713785352d4ea75027b9b92c2c057256be
Alexey Savenkov <alexey.savenkov7@gmail.com>
2021-01-01 23:16:36 +0200

    Update solution1.hs

--- solution1.hs
+++ solution1.hs
@@ -1,4 +1,7 @@
 import Data.List
 
-f::[Integer] -> Integer 
-f xs = head [x*y| (x:rest) <- tails xs, y <- rest, x + y == 2020]
+f1::[Integer] -> Integer
+f1 xs = head [x*y| (x:rest) <- tails xs, y <- rest, x + y == 2020]
+
+f2::[Integer] -> Integer
+f2 xs = head [x*y*z| (x:rest) <- tails xs, (y:rest2) <- tails rest, z <- rest2, x + y + z == 2020]
