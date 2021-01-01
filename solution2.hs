cf245a6a6b1da58ac5d8dc5eb0c6f57f02ae2fbe
Alexey Savenkov <alexey.savenkov7@gmail.com>
2021-01-01 23:55:51 +0200

    Update solution2.hs

--- solution2.hs
+++ solution2.hs
@@ -4,4 +4,10 @@ between::Ord a => a -> (a, a) -> Bool
 between x (mn,mx) = x >= mn && x <= mx 
 
 f::[(Int, Int, Char, String)] -> Int
-f input = length $ filter (\(mn,mx,ch,str) -> ((length . filter (== ch)) str) `between` (mn, mx)) input
+f1 input = length $ filter (\(mn,mx,ch,str) -> ((length . filter (== ch)) str) `between` (mn, mx)) input
+
+get::[a] -> Int -> Maybe a
+get xs i = if i < length xs then Just (xs !! i) else Nothing
+
+f2::[(Int, Int, Char, String)] -> Int
+f2 input = length $ filter (\(mn,mx,ch,str) -> (length . filter (==(Just ch)) $ [get str (mn-1), get str (mx-1)] ) == 1) input
